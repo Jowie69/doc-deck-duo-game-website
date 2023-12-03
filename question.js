1c695c598250e9e3c177fa3d3ed6452e05a2de4c
Jowie69 <eiwojserolf032002@gmail.com>
2023-12-03 23:59:37 +0800

    committed all files

---
+++ question.js
@@ -0,0 +1,445 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which of the following is not a major blood vessel in the cardiovascular system?",
+        answer: "C. Spinal artery",
+        options: [ 
+            "A. Aorta",
+            "B. Carotid artery",
+            "C. Spinal artery",
+            "D. Pulmonary artery"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the main function of the cardiovascular system?",
+        answer: "A. Transportation of oxygen and nutrients",
+        options: [
+            "A. Transportation of oxygen and nutrients",
+            "B. Regulation of body temperature",
+            "C. Production of hormones",
+            "D. Protection against infection"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which chamber of the heart receives oxygenated blood from the lungs?",
+        answer: "A. Left atrium",
+        options: [
+            "A. Left atrium",
+            "B. Left ventricle",
+            "C. Right atrium",
+            "D. Right ventricle"
+        ]
+    },
+    {
+        numb: 4,
+        question: "The cardiovascular system is responsible for the circulation of which of the following?",
+        answer: "A. Blood",
+        options: [
+            "A. Blood",
+            "B. Lymph",
+            "C. Cerebrospinal fluid",
+            "D. Synovial fluid"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of the following is not a type of blood vessel in the cardiovascular system?",
+        answer: "D. Tendon",
+        options: [
+            "A. Artery",
+            "B. Vein",
+            "C. Capillary",
+            "D. Tendon"
+        ]
+    },
+    {
+        numb: 6,
+        question: "The sinoatrial node is responsible for initiating which type of electrical signal in the heart?",
+        answer: "A. Contraction",
+        options: [
+            "A. Contraction",
+            "B. Relaxation",
+            "C. Fibrillation",
+            "D. Arrhythmia"
+        ]
+    },
+    {
+        numb: 7,
+        question: "The process by which blood vessels constrict to increase blood pressure is known as:",
+        answer: "C. Vasoconstriction",
+        options: [
+            "A. Vasodilation",
+            "B. Hemostasis",
+            "C. Vasoconstriction",
+            "D. Thrombosis"
+        ]
+    },
+    {
+        numb: 8,
+        question: "The sound of the heartbeat is caused by the closing of which heart valves?",
+        answer: "B. Semilunar valves",
+        options: [
+            "A. Atrioventricular valves",
+            "B. Semilunar valves",
+            "C. Mitral valve",
+            "D. Tricuspid valve"
+        ]
+    },
+    {
+        numb: 9,
+        question: "The average resting heart rate for adults is approximately:",
+        answer: "A. 60-100 beats per minute",
+        options: [
+            "A. 60-100 beats per minute",
+            "B. 120-160 beats per minute",
+            "C. 30-50 beats per minute",
+            "D. 200-250 beats per minute"
+        ]
+    },
+    {
+        numb: 10,
+        question: "The structure that separates the left and right sides of the heart is called the:",
+        answer: "A. Interventricular septum",
+        options: [
+            "A. Interventricular septum",
+            "B. Interatrial septum",
+            "C. Atrioventricular septum",
+            "D. Atrial septum"
+        ]
+    },
+    {
+        numb: 11,
+        question: "What organ pumps blood throughout the body?",
+        answer: "C. Heart",
+        options: [
+            "A. Brain",
+            "B. Skin",
+            "C. Heart",
+            "D. Lungs"
+        ]
+    },
+    {
+        numb: 12,
+        question: "What is the inner layer of the heart wall?",
+        answer: "C. Endocardium",
+        options: [
+            "A. Epicardium",
+            "B. Myocardium",
+            "C. Endocardium",
+            "D. Pericardium"
+        ]
+    },
+    {
+        numb: 13,
+        question: "It is a rapid heart rate of over 100 beats per minute.",
+        answer: "A. Tachycardia",
+        options: [
+            "A. Tachycardia",
+            "B. Bradycardia",
+            "C. Cardiac",
+            "D. Fibrillation"
+        ]
+    },
+    {
+        numb: 14,
+        question: "What part of the heart pumps blood low in oxygen to the lungs?",
+        answer: "D. Right ventricle",
+        options: [
+            "A. Left Atrium",
+            "B. Right Atrium",
+            "C. Left Ventricle",
+            "D. Right Ventricle"
+        ]
+    },
+     {
+        numb: 15,
+        question: "________ is classified as a large vein, with a wide diameter of up to 2cm and a length of approximately 7 cm.",
+        answer: "B. Superior vena cava",
+        options: [
+            "A. Aorta",
+            "B. Superior vena cava",
+            "C. Pericardium",
+            "D. Sternum"
+        ]
+    },
+    {
+        numb: 16,
+        question: "How many hollow chambers does the heart have?",
+        answer: "A. 4",
+        options: [
+            "A. 4",
+            "B. 6",
+            "C. 5",
+            "D. 3"
+        ]
+    },
+    {
+        numb: 17,
+        question: "It is the lack of adequate oxygen supply to the heart muscle.",
+        answer: "B. Ischemia",
+        options: [
+            "A. Heart Block",
+            "B. Ischemia",
+            "C. Fibrillation",
+            "D. Tachycardia"
+        ]
+    },
+    {
+        numb: 18,
+        question: "The performance of the human heart from the beginning of one heartbeat to the beginning of the next.",
+        answer: "D. Cardiac cycle",
+        options: [
+            "A. Heart Cycle",
+            "B. Diaphragm Cycle",
+            "C. Aorta Cycle",
+            "D. Cardiac Cycle"
+        ]
+    },
+    {
+        numb: 19,
+        question: "What is the main pumping chamber of the heart?",
+        answer: "A. Ventricle",
+        options: [
+            "A. Ventricle",
+            "B. Tricuspid",
+            "C. Ventricle",
+            "D. Systole"
+        ]
+    },
+    {
+        numb: 20,
+        question: "What is the large vessel carrying blood away from the heart?",
+        answer: "C. Artery",
+        options: [
+            "A. Mitral",
+            "B. Carotid",
+            "C. Artery",
+            "D. Vascular system"
+        ]
+    },
+    {
+        numb: 21,
+        question: "Thin-walled vessels where oxygen and nutrients exchange with tissues?",
+        answer: "B. Capillaries",
+        options: [
+            "A. Heart",
+            "B. Capillaries",
+            "C. Pulse",
+            "D. Plasma"
+        ]
+    },
+    {
+        numb: 22,
+        question: "The valve between the left atrium and left ventricle.",
+        answer: "A. Mitral valve",
+        options: [
+            "A. Mitral valve",
+            "B. Artery",
+            "C. Hemoglobin",
+            "D. Vein"
+        ]
+    },
+    {
+        numb: 23,
+        question: "It provides the heart muscle with oxygenated blood.",
+        answer: "A. Coronary circuit",
+        options: [
+            "A. Coronary circuit",
+            "B. Capillaries",
+            "C. Systole",
+            "D. Aldosterone"
+        ]
+    },
+    {
+        numb: 24,
+        question: "The liquid component of the blood.",
+        answer: "B. Plasma",
+        options: [
+            "A. Pressure",
+            "B. Plasma",
+            "C. Pulse",
+            "D. Carotid"
+        ]
+    },
+    {
+        numb: 25,
+        question: "What is the medical term for high blood pressure?",
+        answer: "C. Hypertension",
+        options: [
+            "A. Hyperacidity",
+            "B. Hypersensitivity",
+            "C. Hypertension",
+            "D. Hyperglycemia"
+        ]
+    },
+    {
+        numb: 26,
+        question: "The device that is used to measure blood pressure.",
+        answer: "A. Sphygmomanometer",
+        options: [
+            "A. Sphygmomanometer",
+            "B. Stethoscope",
+            "C. Electrocardiogram",
+            "D. X-ray machine"
+        ]
+    },
+    {
+        numb: 27,
+        question: "The condition where fatty deposits narrow blood vessels.",
+        answer: "B. Atherosclerosis",
+        options: [
+            "A. Diabetic",
+            "B. Atherosclerosis",
+            "C. Coronary artery disease",
+            "D. Carotid artery disease"
+        ]
+    },
+    {
+        numb: 28,
+        question: "It sits under your lungs and separates your chest cavity from your abdomen.",
+        answer: "C. Diaphragm",
+        options: [
+            "A. Thorax",
+            "B. Pericardium",
+            "C. Diaphragm",
+            "D. Apex"
+        ]
+    },
+    {
+        numb: 29,
+        question: "The apex of the heart is oriented towards the?",
+        answer: "C. Left hip",
+        options: [
+            "A. Right hip",
+            "B. Right shoulder",
+            "C. Left hip",
+            "D. Umbilicus"
+        ]
+    },
+    {
+        numb: 30,
+        question: "It fills the space between the layers of pericardium.",
+        answer: "C. Serous Fluid",
+        options: [
+            "A. Interventricular septum",
+            "B. Pulmonary",
+            "C. Serous fluid",
+            "D. Tricuspid valve"
+        ]
+    },
+    {
+        numb: 31,
+        question: "Inflammation of the endocardium which may lead to valvular incompetence.",
+        answer: "A. Endocarditis",
+        options: [
+            "A. Endocarditis",
+            "B. Pericarditis",
+            "C. Atria",
+            "D. Septum"
+        ]
+    },
+    {
+        numb: 32,
+        question: "A wall that divides the heart sagittally into right and left sides.",
+        answer: "B. Septum",
+        options: [
+            "A. AV valves",
+            "B. Septum",
+            "C. Tricuspid",
+            "D. Valves"
+        ]
+    },
+    {
+        numb: 33,
+        question: "The narrowing of blood vessels is known as?",
+        answer: "D. Vasoconstriction",
+        options: [
+            "A. Endocardium",
+            "B. Aorta",
+            "C. Vasodilation",
+            "D. Vasoconstriction"
+        ]
+    },
+    {
+        numb: 34,
+        question: "Branch from the aorta to supply the heart muscle with oxygenated blood.",
+        answer: "C. Coronary arteries",
+        options: [
+            "A. Cardiac Veins",
+            "B. Coronary sinus",
+            "C. Coronary arteries",
+            "D. Pulmonary Trunk"
+        ]
+    },
+    {
+        numb: 35,
+        question: "The larger blood vessels that carry blood away from the heart are called?",
+        answer: "D. Arteries",
+        options: [
+            "A. Cardiac veins",
+            "B. Aorta",
+            "C. Tachycardia",
+            "D. Arteries"
+        ]
+    },
+    {
+        numb: 36,
+        question: "It is when the blood flows from the right side of the heart to the lungs and back to the left side of the heart.",
+        answer: "B. Pulmonary circulation",
+        options: [
+            "A. Systemic circulation",
+            "B. Pulmonary circulation",
+            "C. Diastole",
+            "D. Fibrillation"
+        ]
+    },
+    {
+        numb: 37,
+        question: "A rapid, uncoordinated shuddering of the heart muscles resulting from ischemia.",
+        answer: "C. Fibrillation",
+        options: [
+            "A. Heart block",
+            "B. Systole",
+            "C. Fibrillation",
+            "D. Systematic circulation"
+        ]
+    },
+    {
+        numb: 38,
+        question: "What is the cardiovascular system responsible for?",
+        answer: "A. Transporting oxygen and nutrients throughout the body and removing waste products",
+        options: [
+            "A. Transporting oxygen and nutrients throughout the body and removing waste products",
+            "B. Controlling the body's temperature",
+            "C. Protecting the body from infection",
+            "D. Producing hormones"
+        ]
+    },
+    {
+        numb: 39,
+        question: "Which of the following is NOT a component of the cardiovascular system?",
+        answer: "B. Lungs",
+        options: [
+            "A. Heart",
+            "B. Lungs",
+            "C. Blood vessels",
+            "D. Blood"
+        ]
+    },
+    {
+        numb: 40,
+        question: "What is the main function of the heart?",
+        answer: "A. To pump blood throughout the body",
+        options: [
+            "A. To pump blood throughout the body",
+            "B. To filter the blood",
+            "C. To produce oxygen",
+            "D. To store blood"
+        ]
+    }
+];
+
+
+
